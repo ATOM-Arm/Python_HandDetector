4661adf9bc84403a71f20e5d5a8a0ead7796fc1d
Enzo Ribas <enzorochaleitedinizribas@gmail.com>
2025-10-06 20:14:10 -0300

    .

---
+++ activate_env.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Script para ativar o ambiente virtual do projeto Python Hand Detector
+
+echo "Ativando ambiente virtual Python Hand Detector..."
+source .venv/bin/activate
+echo "Ambiente virtual ativado com sucesso!"
+echo "Python version: $(python --version)"
+echo "Para desativar, use: deactivate"
